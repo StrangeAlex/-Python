496e81bc1e2cfa7725bc76354f7d790af006ab4e
StrangeAlex <szaburdaev@me.com>
2020-01-24 19:54:40 +0300

    hi

---
+++ n3.py
@@ -0,0 +1,50 @@
+# def divide(a, b):
+#     try:
+#         return a/b
+#     except ZeroDivisionError:
+#         return
+#
+# print(divide(int(input("Enter 1st number: ")), int(input("Enter 2nd number: "))))
+
+
+# def info(name, surname, yearOfBirth, city, email, phoneNumber):
+#     return name, surname, yearOfBirth, city, email, phoneNumber #Простите, что так топорно, просто времени очень мало осталось, а я синтаксис забыл :(
+# print(info(str(input("Enter your name ")), str(input("Enter your surname ")), str(input("Enter your year of birth ")), str(input("Enter your city ")), str(input("Enter your email ")),str(input("Enter your phoneNumber "))))
+
+
+# def myFunc(a, b, c):
+#     array = [a, b, c]
+#     max1 = max(array)
+#     array.remove(max(array))
+#     max2 = max(array)
+#     return max1 + max2
+# print(myFunc(2, 3, 5))
+
+
+# def myFunc(x, y):
+#     xStart = x
+#     for i in range(y - 1):
+#         x *= xStart
+#     return(x)
+# print(myFunc(3, 2))
+
+
+
+# def myFunc (text):
+#     print(text.title())
+#     return
+# myFunc("hEllO world!")
+
+# a = []
+# while True:
+#     a = list(input())
+#     if Q not in a:
+#         for i in range(len(a)):
+#             if a[i] == ' ':
+#                 a.remove(a[i])
+#             else:
+#                 a[i] = int(a[i])
+#     else:
+#         break
+#     a.append(num)
+# print(a)
